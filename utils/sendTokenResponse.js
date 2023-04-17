9d4c63079bd3511c7430459d43c143fd59333e12
MrBennySolomon <MrBennySolomon@gmail.com>
2023-04-17 09:24:34 +0300

    .

--- utils/sendTokenResponse.js
+++ utils/sendTokenResponse.js
@@ -11,11 +11,11 @@ const sendTokenResponse = (user, statusCode, res) => {
   }
 
   const options = {
-    // expires: new Date(
-    //   // Convert the 30 days in the config t0 30 days in milliseconds
-    //   Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    // ),
-    // httpOnly: true
+    expires: new Date(
+      // Convert the 30 days in the config t0 30 days in milliseconds
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
   };
 
   // Send secure cookie in production
